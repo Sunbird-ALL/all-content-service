912e954b6438093a07a60091fc933a257120a121
DevendraPPatil <devendra.patil@tekditechnologies.com>
2024-11-11 11:39:24 +0530

    Fetch: Health api added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
-  NestFastifyApplication,
+  NestFastifyApplication
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppClusterService } from './app-cluster.service';
 import compression from '@fastify/compress';
+import fastifyMultipart from 'fastify-multipart';
+import { FastifyReply, FastifyRequest } from 'fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -15,13 +17,13 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
-  await app.register(compression,{
+  await app.register(compression, {
     global: true,
     zlibOptions: {
       level: 6,
     },
     threshold: 512,
-    encodings: ['gzip', 'deflate']
+    encodings: ['gzip', 'deflate'],
   });
 
   app.useGlobalPipes(new ValidationPipe());
@@ -44,6 +46,20 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // Register fastify-multipart plugin
+  app.register(fastifyMultipart);
+
+  // Access the Fastify instance to define the health check endpoint
+  const fastifyInstance = app.getHttpAdapter().getInstance();
+
+  fastifyInstance.get('/ping', async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.status(200).send({
+      status: true,
+      message: 'content ServiceApp is working',
+    });
+  });
+
   await app.listen(3008, '0.0.0.0');
 }
+
 AppClusterService.clusterize(bootstrap);

1b1ad69d720a85f4db1614348b78c461fe8e970c
DevendraPPatil <devendra.patil@tekditechnologies.com>
2024-11-12 09:44:23 +0530

    Fix:Comment resolved

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
-  NestFastifyApplication
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,20 +9,19 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppClusterService } from './app-cluster.service';
 import compression from '@fastify/compress';
 
-
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
 
-  await app.register(compression, {
+  await app.register(compression,{
     global: true,
     zlibOptions: {
       level: 6,
     },
     threshold: 512,
-    encodings: ['gzip', 'deflate'],
+    encodings: ['gzip', 'deflate']
   });
 
   app.useGlobalPipes(new ValidationPipe());
@@ -47,5 +46,4 @@ async function bootstrap() {
 
   await app.listen(3008, '0.0.0.0');
 }
-
 AppClusterService.clusterize(bootstrap);
